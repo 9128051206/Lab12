56eacd80e9e739a85d39ea1afea64bff4ff8e314
Юлия Шепелева <9128051206@mail.ru>
2022-01-27 19:08:22 +0500

    Добавьте файлы проекта.

---
+++ Lab12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double length =  Сircle.GetLength(radius);    
+            double square = Сircle.GetSquare(radius);
+            string point = Сircle.GetPoint(x0, y0, x, y,radius);
+            Console.WriteLine($"{length} \n{square} \n{point}");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Lab12/Сircle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    static class Сircle
+    {
+        static public double GetLength(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        static public double GetSquare(double radius)
+        {
+            return Math.PI* radius *radius;
+        }
+        static public string GetPoint(double x0, double y0, double x, double y, double radius)
+        {
+            double d = Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2));
+            if (d <= radius)
+                return  "Точка принадлежит";
+            else
+                return "Точка не принадлежит";
+        }
+    }
+}
